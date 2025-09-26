76a1ad3d54b3409e75b580b94e518c843b75a855
moiz <moiz@blueorbits.com>
2025-09-26 14:56:15 +0500

    initial-commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['**/*'], // Ignore everything
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
